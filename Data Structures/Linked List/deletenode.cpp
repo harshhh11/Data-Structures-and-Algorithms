9e02f88504f4e29e638678175b08858b04788895
harsh <harshbhadu8@gmail.com>
2022-11-14 21:06:47 +0530

    Added Delete Node at Nth Position

--- Data Structures/Linked List/deletenode.cpp
+++ Data Structures/Linked List/deletenode.cpp
@@ -39,6 +39,22 @@ void Print() // Print all elements in the list
 	cout << "\n";
 }
 
+void Delete(int n) {  //
+	Node* headRef = head;
+	if (n == 1) {
+		head = headRef->next; // head points to second node
+		delete headRef;
+		return;
+	}
+	for (int i = 0; i < n-2; i++) 
+		headRef = headRef->next;
+	// headRef points to (n-1)th Node
+	Node* nNode = headRef->next; // nth node
+	headRef->next = nNode->next; // (n+1)th node
+	delete nNode;
+}
+
+
 int main() {
 	head = NULL;
 	Insert(2);
@@ -46,11 +62,11 @@ int main() {
 	Insert(6);
 	Insert(5); // List : 2, 4, 6, 5
 	Print();
-	// int n;
-	// cout << "Enter a position\n";
-	// cin >> n;
-	// Delete(n); 
-	// Print();
+	int n;
+	cout << "Enter a position\n";
+	cin >> n;
+	Delete(n); 
+	Print();
 }
 
 
