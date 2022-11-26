9e583bc0996699db7c99074d836f7abe0946d8a0
harsh <harshbhadu86@gmail.com>
2022-11-26 13:53:01 +0530

    Added Level Order Travesal

---
+++ Data Structures/Binary Trees/LevelOrderTraversal.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+typedef struct Node{
+    char data;
+    Node* left;
+    Node* right;
+} Node;
+
+void LevelOrder(Node* root) {
+    if (root == NULL) return;
+    queue<Node*> Q;
+    Q.push(root);
+    // While there is atleast one discovered node
+    while (!Q.empty()) {
+        Node* current = Q.front();
+        cout << current->data << " ";
+        if(current->left != NULL) Q.push(current->left);
+        if(current->right != NULL) Q.push(current->right);
+        Q.pop(); // Removing the element at front which is printed 
+    }
+}
+
+// Time Complexity = O(n) for all
+// Space Complexity = O(n) - worst/avg , O(1) - best (for left aligned tree)
